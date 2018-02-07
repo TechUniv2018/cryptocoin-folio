bae42e8f8cf15ab7b51e93e4a333e4bfcc302412
ShubhamZanwar <zanwar.shubham@gmail.com>
2018-02-07 12:52:01 +0530

    LiveGraphHandler: The function is tested to return a resolved promise

--- test/index.test.js
+++ test/index.test.js
@@ -40,7 +40,10 @@ describe('Testing the Live Graph Handler function', () => {
   test('The function must return a Promise', () => {
     expect(LiveGraphHandler('BTC')).toBeInstanceOf(Promise);
   });
-  test('The function should return a resolved promise', () => {
-    expect(LiveGraphHandler()).resolves.not.toBeUndefined();
+  test('The function should return a resolved promise', (done) => {
+    LiveGraphHandler().then(() => {
+      expect(true).toBe(true);
+      done();
+    });
   });
 });
