0c512c7bbbcda3808a80460f2eae557276d06496
Rahul Kumar <rahulcomp24@gmail.com>
2018-02-07 12:24:33 +0530

    LiveDataTicker : Update Links

--- src/externalLinks.js
+++ src/externalLinks.js
@@ -1,4 +1,4 @@
 module.exports = {
-  EXTERNAL_LIVE_PRICE: 'https://min-api.cryptocompare.com/data/price?fsym=USD&tsyms=BTC,ETH,XRP,LTC,STR,XMR,ZEC,BCH,BTC,ETC',
-  EXTERNAL_LIVE_PRICE_DETAIL = 'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=USD&tsyms=BTC,ETH,XRP,LTC,STR,XMR,ZEC,BCH,BTC,ETC'
+  EXTERNAL_LIVE_PRICE: 'https://min-api.cryptocompare.com/data/price?fsyms=BTC,ETH,XRP,LTC,STR,XMR,ZEC,BCH,BTS,ETC&tsyms=USD',
+  EXTERNAL_LIVE_PRICE_DETAIL: 'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,XRP,LTC,STR,XMR,ZEC,BCH,BTS,ETC&tsyms=USD',
 };
