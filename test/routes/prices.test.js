f3f2e0e02f0613716ef2e07d615b4d2e90ebee59
parth <srivastavaparth2016@gmail.com>
2018-02-08 18:03:35 +0530

    Test case added to check if the response contains deatils of 10 coins or not

--- test/routes/prices.test.js
+++ test/routes/prices.test.js
@@ -12,4 +12,19 @@ describe('testing server', () => {
       done();
     });
   });
+
+  test('Response should contain 10 cryptocoins data', (done) => {
+    const options = {
+      method: 'GET',
+      url: '/prices',
+    };
+    const coinsCount = (payload) => {
+      const coins = Object.keys(JSON.parse(payload));
+      return coins.length;
+    };
+    Server.inject(options, (response) => {
+      expect(coinsCount(response.payload)).toBe(10);
+      done();
+    });
+  });
 });
