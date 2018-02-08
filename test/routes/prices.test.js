82a375ecb405fa73606864e2db33639f310584f2
parth <srivastavaparth2016@gmail.com>
2018-02-08 18:22:32 +0530

    Test case for checking if the response has all the details needed for each coin and no consisting inconsistencies

--- test/routes/prices.test.js
+++ test/routes/prices.test.js
@@ -27,4 +27,37 @@ describe('testing server', () => {
       done();
     });
   });
+
+  test('Response should contain price, volume and change', (done) => {
+    const options = {
+      method: 'GET',
+      url: '/prices',
+    };
+    const coinsDetails = (payload) => {
+      const payloadJson = JSON.parse(payload);
+      const coins = Object.keys(payloadJson);
+      let countForPrice = 0;
+      let countForVolume = 0;
+      let countForChange = 0;
+      for (let i = 0; i < coins.length; i += 1) {
+        if (payloadJson[coins[i]].Price) {
+          countForPrice += 1;
+        }
+        if (payloadJson[coins[i]].Volume) {
+          countForVolume += 1;
+        }
+        if (payloadJson[coins[i]].Change) {
+          countForChange += 1;
+        }
+      }
+      if (countForChange === 10 && countForPrice === 10 && countForVolume === 10) {
+        return true;
+      }
+      return false;
+    };
+    Server.inject(options, (response) => {
+      expect(coinsDetails(response.payload)).toBe(true);
+      done();
+    });
+  });
 });
