72c93a42381e351f9126fd494951db5d3bb819a3
Ram Prasad Agarwal <ramprasadagarwal313@gmail.com>
2018-02-12 17:09:05 +0530

    Pass: Invalid fullname in signup form

--- test/signup.test.js
+++ test/signup.test.js
@@ -1,26 +1,25 @@
-// const signup = require('../src/routes/signup');
 const Server = require('../index.js');
 
-describe('Test for signup API with valid data', () => {
-  const options = {
-    method: 'POST',
-    url: '/signup',
-    payload: {
-      fullName: 'Jack Mark',
-      email: 'jackmark@alibababa.com',
-      password: 'sample',
-      confirmPassword: 'sample',
-      mobileNumber: 9876543210,
-    },
-  };
+const options = {
+  method: 'POST',
+  url: '/signup',
+};
 
+describe('Test for signup API with valid data', () => {
+  options.payload = {
+    fullName: 'Jack Mark',
+    email: 'jackmark@alibababa.com',
+    password: 'sample',
+    confirmPassword: 'sample',
+    mobileNumber: 9876543210,
+  };
   test('Response code should be 200 for valid signup API call', (done) => {
     Server.inject(options, (response) => {
       expect(response.statusCode).toBe(200);
       done();
     });
   });
-  test('Response code should be 200 for valid signup API call', (done) => {
+  test('Response message should be valid input for valid signup API call', (done) => {
     Server.inject(options, (response) => {
       expect(response.result).toBe('Valid Input');
       done();
@@ -29,26 +28,30 @@ describe('Test for signup API with valid data', () => {
 });
 
 describe('Test for signup API with invalid data', () => {
-  const options = {
-    method: 'POST',
-    url: '/signup',
-    payload: {
+  test('Should pass for invalid mobile number', (done) => {
+    options.payload = {
       fullName: 'Jack Mark',
       email: 'jackmark@alibababa.com',
       password: 'sample',
       confirmPassword: 'sample',
       mobileNumber: 987654321,
-    },
-  };
-
-  test('Response code should be 200 for valid signup API call', (done) => {
+    };
     Server.inject(options, (response) => {
       expect(response.statusCode).toBe(400);
+      expect(response.result).toBe('Invalid Input');
       done();
     });
   });
-  test('Response code should be 200 for valid signup API call', (done) => {
+  test('Should pass for invalid full name', (done) => {
+    options.payload = {
+      fullName: 123456,
+      email: 'jackmark@alibababa.com',
+      password: 'sample',
+      confirmPassword: 'sample',
+      mobileNumber: 987654321,
+    };
     Server.inject(options, (response) => {
+      expect(response.statusCode).toBe(400);
       expect(response.result).toBe('Invalid Input');
       done();
     });
