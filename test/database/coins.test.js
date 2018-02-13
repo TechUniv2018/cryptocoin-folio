c68404bb7acdf6886aa4c54513c192480334c32c
Rahul Kumar <rahulcomp24@gmail.com>
2018-02-13 17:40:30 +0530

    Database : Add more test cases for coins table

--- test/database/coins.test.js
+++ test/database/coins.test.js
@@ -7,4 +7,29 @@ describe('test coin table', () => {
       done();
     });
   });
+
+  test('insert coin into coins table', (done) => {
+    const coinObj = {
+      symbol: 'BTC',
+      name: 'Bitcoin',
+    };
+
+    Models.coins.create(coinObj).then((result) => {
+      const resultCoinObject = {
+        symbol: result.dataValues.symbol,
+        name: result.dataValues.name,
+      };
+      expect(resultCoinObject).toEqual(coinObj);
+      done();
+    });
+  });
+
+  test('delete coin from coins table', (done) => {
+    Models.coins.destroy({ where: { id: 1 }, cascade: true }).then(() => {
+      Models.coins.findAll().then((result) => {
+        expect(result).toEqual([]);
+        done();
+      });
+    });
+  });
 });
