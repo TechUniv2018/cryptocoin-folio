c4505d282b70ae463278c8e54120a45640990cc7
Rahul Kumar <rahulcomp24@gmail.com>
2018-02-14 19:11:18 +0530

    Database : Add beforeAll and afterAll in Transaction test case

--- test/database/transactions.test.js
+++ test/database/transactions.test.js
@@ -1,9 +1,26 @@
 const Models = require('../../models');
 
+const cleanUpAllTables = () => [Models.prices.destroy({ cascade: true, truncate: true }),
+  Models.transactions.destroy({ cascade: true, truncate: true }),
+  Models.coins.destroy({ cascade: true, truncate: true }),
+  Models.users.destroy({ cascade: true, truncate: true })];
+
 describe('test transaction table', () => {
   test('test transactions table is created', (done) => {
     Models.transactions.findAll().then((result) => {
       expect(result).toEqual([]);
+      done();
+    });
+  });
+
+  beforeAll((done) => {
+    Promise.all(cleanUpAllTables()).then(() => {
+      done();
+    });
+  });
+
+  afterAll((done) => {
+    Promise.all(cleanUpAllTables()).then(() => {
       done();
     });
   });
