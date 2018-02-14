8ad0de75e5ac147cafd7a56e6d53030abfcc9651
Rahul Kumar <rahulcomp24@gmail.com>
2018-02-14 20:51:54 +0530

    Database : Add transactions test case to check foreign key constraint

--- test/database/transactions.test.js
+++ test/database/transactions.test.js
@@ -13,6 +13,65 @@ describe('test transaction table', () => {
     });
   });
 
+  test('insert transaction into transaction table should fail with Sequelize Foreign Key Constraint Error', (done) => {
+    const transactionObject = {
+      coinId: 1,
+      userId: 1,
+      price: 321.3213,
+      quantity: 10.12,
+    };
+
+    Models.transactions.create(transactionObject).then().catch((e) => {
+      expect(e.name).toBe('SequelizeForeignKeyConstraintError');
+      done();
+    });
+  });
+
+  test('insert transaction into transaction table should fail with Sequelize Foreign Key Constraint Error - Coin does not exist', (done) => {
+    const userObject = {
+      fullName: 'Jack Mark',
+      email: 'jackmark@alibababa.com',
+      password: 'sample',
+      confirmPassword: 'sample',
+      mobileNumbe: 9876543210,
+    };
+
+    Models.users.create(userObject).then((userResult) => {
+      const transactionObject = {
+        coinId: 1,
+        userId: userResult.dataValues.id,
+        price: 321.3213,
+        quantity: 10.12,
+      };
+
+      Models.transactions.create(transactionObject).then().catch((e) => {
+        expect(e.name).toBe('SequelizeForeignKeyConstraintError');
+        done();
+      });
+    });
+  });
+
+  test('insert transaction into transaction table should fail with Sequelize Foreign Key Constraint Error - User does not exist', (done) => {
+    const coinObj = {
+      symbol: 'BTC',
+      name: 'Bitcoin',
+    };
+
+    Models.coins.create(coinObj).then((coinResult) => {
+      const transactionObject = {
+        coinId: coinResult.dataValues.id,
+        userId: 1,
+        price: 321.3213,
+        quantity: 10.12,
+      };
+
+      Models.transactions.create(transactionObject).then().catch((e) => {
+        expect(e.name).toBe('SequelizeForeignKeyConstraintError');
+        done();
+      });
+    });
+  });
+
   beforeEach((done) => {
     Promise.all(cleanUpAllTables()).then(() => {
       done();
