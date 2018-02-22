6ac467da1b2d8dc7f7920e3a3ae3b879d4db530b
ShubhamZanwar <zanwar.shubham@gmail.com>
2018-02-23 00:29:23 +0530

    ObjToArr: modifying the helper to segment the prices and volume seperately

--- src/utils/helpers/objToArr.js
+++ src/utils/helpers/objToArr.js
@@ -1,12 +1,21 @@
 module.exports = (prices) => {
-  const pricesArr = prices.map(price => [
-    price.time,
-    price.open,
-    price.high,
-    price.low,
-    price.close,
-    price.volumeto,
-  ]);
-  return pricesArr;
+  const pricesArr = [];
+  const volumeArr = [];
+  for (let i = 0; i < prices.length; i += 1) {
+    pricesArr.push([
+      prices[i].time * 1000,
+      prices[i].open,
+      prices[i].high,
+      prices[i].low,
+      prices[i].close,
+    ]);
+    volumeArr.push([
+      prices[i].time * 1000,
+      prices[i].volumeto,
+    ]);
+  }
+  return {
+    prices: pricesArr,
+    volume: volumeArr,
+  };
 };
-
