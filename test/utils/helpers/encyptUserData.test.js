d2484927ce0a9fa2d74f936f287644c0903c28df
Ram Prasad Agarwal <ramprasadagarwal313@gmail.com>
2018-02-25 23:35:26 +0530

    encryptUserData helper funtion object keys test

--- test/utils/helpers/encyptUserData.test.js
+++ test/utils/helpers/encyptUserData.test.js
@@ -12,4 +12,15 @@ describe('Test for encryptUserdData helper function', () => {
     expect(typeof encryptUserdData(options)).toBe('object');
     done();
   });
+  test('Should return user data object', (done) => {
+    const options = {
+      fullName: 'Jack Mark',
+      email: 'jackmark@alibababa.com',
+      password: 'sample12',
+      confirmPassword: 'sample',
+      mobileNumbe: 9876543210,
+    };
+    expect(Object.keys(encryptUserdData(options))).toEqual(['fullName', 'email', 'mobileNumbe', 'password']);
+    done();
+  });
 });
