827a9db0e718247d8a1a716bec073aad62405024
Ram Prasad Agarwal <ramprasadagarwal313@gmail.com>
2018-02-25 23:45:31 +0530

    getUserFormPayload string to object test

--- test/utils/helpers/getUserFormPayload.test.js
+++ test/utils/helpers/getUserFormPayload.test.js
@@ -13,4 +13,16 @@ describe('Test for getUserFormPayload helper function', () => {
     expect(typeof getUserFormPayload(options)).toBe('object');
     done();
   });
+  test('Should return user data object', (done) => {
+    const options = {};
+    options.payload = JSON.stringify({
+      fullName: 'Jack Mark',
+      email: 'jackmark@alibababa.com',
+      password: 'sample12',
+      confirmPassword: 'sample',
+      mobileNumbe: 9876543210,
+    });
+    expect(typeof getUserFormPayload(options)).toBe('object');
+    done();
+  });
 });
