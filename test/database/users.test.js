6cad00703db26f2f163ef5aa246a9dd11d9238f4
Ram Prasad Agarwal <ramprasadagarwal313@gmail.com>
2018-02-25 23:15:49 +0530

    beforeAll add

--- test/database/users.test.js
+++ test/database/users.test.js
@@ -1,6 +1,14 @@
 const Models = require('../../models');
 
 describe('test user table', () => {
+  beforeAll((done) => {
+    Models.users.destroy({
+      truncate: true,
+      cascade: true,
+    }).then(() => {
+      done();
+    }).catch(console.log);
+  });
   test('test users table is created', (done) => {
     Models.users.findAll().then((result) => {
       expect(result).toEqual([]);
