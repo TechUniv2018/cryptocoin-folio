72ea84202ae98472fecab521dd3a511ad2613009
Ram Prasad Agarwal <ramprasadagarwal313@gmail.com>
2018-02-25 23:47:35 +0530

    object keys check

--- test/utils/helpers/getUserFormPayload.test.js
+++ test/utils/helpers/getUserFormPayload.test.js
@@ -13,7 +13,7 @@ describe('Test for getUserFormPayload helper function', () => {
     expect(typeof getUserFormPayload(options)).toBe('object');
     done();
   });
-  test('Should return user data object', (done) => {
+  test('Should return user data object from string', (done) => {
     const options = {};
     options.payload = JSON.stringify({
       fullName: 'Jack Mark',
@@ -23,6 +23,19 @@ describe('Test for getUserFormPayload helper function', () => {
       mobileNumbe: 9876543210,
     });
     expect(typeof getUserFormPayload(options)).toBe('object');
+    done();
+  });
+  test('Should return user data object from string', (done) => {
+    const options = {};
+    options.payload = {
+      fullName: 'Jack Mark',
+      email: 'jackmark@alibababa.com',
+      password: 'sample12',
+      confirmPassword: 'sample',
+      mobileNumbe: 9876543210,
+    };
+    expect(Object.keys(getUserFormPayload(options)))
+      .toEqual(['fullName', 'email', 'password', 'confirmPassword', 'mobileNumbe']);
     done();
   });
 });
