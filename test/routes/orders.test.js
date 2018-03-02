1353bd74afab9178519ee2b3b5f4976629d7e825
parth <srivastavaparth2016@gmail.com>
2018-03-02 09:46:41 +0530

    adding test case for order route for other coins

--- test/routes/orders.test.js
+++ test/routes/orders.test.js
@@ -2,10 +2,21 @@ const Server = require('../../index');
 
 
 describe('testing server', () => {
-  test('Response code should be 200', (done) => {
+  test('Response code should be 200 for BTC', (done) => {
     const options = {
       method: 'GET',
       url: '/orders/BTC',
+    };
+
+    Server.inject(options, (response) => {
+      expect(response.statusCode).toBe(200);
+      done();
+    });
+  });
+  test('Response code should be 200 for other coins', (done) => {
+    const options = {
+      method: 'GET',
+      url: '/orders/LTC',
     };
 
     Server.inject(options, (response) => {
