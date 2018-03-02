114060ee7c0167cf0d04a5e8ee50f776fd63e239
parth <srivastavaparth2016@gmail.com>
2018-03-02 09:40:26 +0530

    adding test case to check if promise or not

--- test/handlers/orderbooks.test.js
+++ test/handlers/orderbooks.test.js
@@ -1,6 +1,9 @@
 const orderBooks = require('../../src/handlers/orderBooks');
 
 describe('testing the orderbooks function', () => {
+  test('checking if the function returns a promise or not', () => {
+    expect(orderBooks('BTC')).toBeInstanceOf(Promise);
+  });
   test('the function should return an object when BTC is supplied', (done) => {
     orderBooks('BTC').then(data => expect(typeof data).toBe('object'));
     done();
@@ -23,6 +26,12 @@ describe('testing the orderbooks function', () => {
   });
   test('the function should return an object with 3 keys for each item for BTC', (done) => {
     orderBooks('BTC').then((data) => {
+      expect((Object.keys(data.asks[0]).length)).toBe(3);
+      done();
+    });
+  });
+  test('the function should return an object with 3 keys for each item for other coin', (done) => {
+    orderBooks('LTC').then((data) => {
       expect((Object.keys(data.asks[0]).length)).toBe(3);
       done();
     });
