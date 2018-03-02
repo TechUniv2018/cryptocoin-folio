66638fc6497f41e2ba062303d264b375cb13e56a
parth <srivastavaparth2016@gmail.com>
2018-03-02 09:19:15 +0530

    adding test case for orderbook function

--- test/handlers/orderbooks.test.js
+++ test/handlers/orderbooks.test.js
@@ -1,14 +1,22 @@
 const orderBooks = require('../../src/handlers/orderBooks');
 
 describe('testing the orderbooks function', () => {
-  test('the function should return an object when BTC is supplied', () => {
+  test('the function should return an object when BTC is supplied', (done) => {
     orderBooks('BTC').then(data => expect(typeof data).toBe('object'));
+    done();
   });
-  test('the function should return an object when any other coin is supplied other than btc', () => {
+  test('the function should return an object when any other coin is supplied other than btc', (done) => {
     orderBooks('LTC').then(data => expect(typeof data).toBe('object'));
+    done();
   });
   test('the function should return an object with two keys for BTC', (done) => {
     orderBooks('BTC').then((data) => {
+      expect((Object.keys(data)).length).toBe(2);
+      done();
+    });
+  });
+  test('the function should return an object with two keys for BTC', (done) => {
+    orderBooks('LTC').then((data) => {
       expect((Object.keys(data)).length).toBe(2);
       done();
     });
