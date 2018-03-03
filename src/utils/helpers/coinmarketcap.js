40cca7d873d77da5525e62727d1a1923295527a5
parth <srivastavaparth2016@gmail.com>
2018-03-03 12:47:13 +0530

    adding fifty coin capability supposrted by binance as well

--- src/utils/helpers/coinmarketcap.js
+++ src/utils/helpers/coinmarketcap.js
@@ -2,15 +2,40 @@ const axios = require('axios');
 const fs = require('fs');
 
 
-const coinmarketcap = () => axios.get('https://api.coinmarketcap.com/v1/ticker/?start=0&limit=50').then((data) => {
+const coinmarketcap = coins => axios.get('https://api.coinmarketcap.com/v1/ticker/?start=0&limit=200').then((data) => {
   const dataArray = data.data;
   const obj = {};
+  let objLength = 0;
   for (let i = 0; i < dataArray.length; i += 1) {
-    obj[dataArray[i].symbol] = dataArray[i].name;
+    for (let j = 0; j < coins.length; j += 1) {
+      if (dataArray[i].symbol === coins[j]) {
+        obj[dataArray[i].symbol] = dataArray[i].name;
+        objLength += 1;
+        break;
+      }
+    }
+    if (objLength === 50) {
+      console.log(objLength);
+      break;
+    }
   }
   return obj;
 });
 
-coinmarketcap().then((data) => {
-  fs.writeFile('../constants/coin-dictionary.js', `module.exports =${JSON.stringify(data)}`);
+const binance = () => axios.get('https://api.binance.com/api/v1/ticker/allPrices').then((data) => {
+  const coins = ['BTC'];
+  const dataArray = data.data;
+  for (let i = 0; i < dataArray.length; i += 1) {
+    const symbol = dataArray[i].symbol;
+    if (symbol.indexOf('BTC') > 0) {
+      coins.push(symbol.slice(0, symbol.indexOf('BTC')));
+    }
+  }
+  console.log(coins);
+  coinmarketcap(coins).then((dataToWrite) => {
+    fs.writeFile('../constants/coin-dictionary.js', `module.exports =${JSON.stringify(dataToWrite)}`);
+  });
 });
+
+binance();
+
