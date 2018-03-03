6a379424042142b8864a7c760cabe325d5aef4b4
parth <srivastavaparth2016@gmail.com>
2018-03-03 07:28:38 +0530

    adding those 50 coins to the api call url usinf destructuring

--- src/utils/constants/externalLinks.js
+++ src/utils/constants/externalLinks.js
@@ -1,4 +1,7 @@
+const coins = Object.keys(require('./coin-dictionary'));
+
+console.log(...coins);
 module.exports = {
-  EXTERNAL_LIVE_PRICE: 'https://min-api.cryptocompare.com/data/price?fsyms=BTC,ETH,XRP,LTC,STR,XMR,ZEC,BCH,BTS,ETC&tsyms=USD',
-  EXTERNAL_LIVE_PRICE_DETAIL: 'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,XRP,LTC,NEO,XMR,ZEC,BCH,BTS,ETC&tsyms=USD',
+  EXTERNAL_LIVE_PRICE: 'https://min-api.cryptocompare.com/data/price?fsyms=BTC,ETH,XRP,LTC,NEO,XMR,ZEC,BCH,BTS,ETC&tsyms=USD',
+  EXTERNAL_LIVE_PRICE_DETAIL: `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${[...coins]}&tsyms=USD`,
 };
