e1cf85439b1fcdd0ff378aaf701a7f2a5a4282fd
Ram Prasad Agarwal <ramprasadagarwal313@gmail.com>
2018-03-06 03:49:02 +0530

    jwtKey: add as constant

--- src/utils/constants/index.js
+++ src/utils/constants/index.js
@@ -1,10 +1,12 @@
 const coins = require('./coins');
 const externalLinks = require('./externalLinks');
 const namesDict = require('./SymbolsToCoins');
+const jwtKey = require('./jwtKey');
 
 module.exports = {
   coins,
   externalLinks,
   namesDict,
+  jwtKey,
 };
 
