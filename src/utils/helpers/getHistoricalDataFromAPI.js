dc794ba2d44b8c343839f82fee502ea8dd1c8687
Rahul Kumar <rahulcomp24@gmail.com>
2018-03-07 23:55:07 +0530

    FIX: getHistoricalDataFromAPI

--- src/utils/helpers/getHistoricalDataFromAPI.js
+++ src/utils/helpers/getHistoricalDataFromAPI.js
@@ -1,15 +1,20 @@
 const EXTERNAL_LINKS = require('./../constants/externalLinks');
 const rp = require('request-promise');
 
-module.exports = (coinObj) => {
-  const options = {
-    url: EXTERNAL_LINKS.EXTERNAL_PRICE_HIST_MIN,
-    method: 'get',
-    qs: {
-      fsym: 'USD',
-      tsym: coinObj.symbol,
-    },
-  };
-  return rp(options);
+module.exports = (coinsArray) => {
+  const priceRequestPromisesArray = [];
+  coinsArray.forEach((coinObj) => {
+    const options = {
+      url: EXTERNAL_LINKS.EXTERNAL_PRICE_HIST_MIN,
+      method: 'get',
+      qs: {
+        fsym: 'USD',
+        tsym: coinObj.symbol,
+      },
+    };
+    console.log(options);
+    priceRequestPromisesArray.push(rp(options).then(result => ({ result: JSON.parse(result), coinId: coinObj.id })));
+  });
+  return priceRequestPromisesArray;
 };
 
