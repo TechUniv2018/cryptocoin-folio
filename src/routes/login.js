919db9d7bc92cddaef5f2d19f32c380ba824a5b8
Ram Prasad Agarwal <ramprasadagarwal313@gmail.com>
2018-03-10 15:04:04 +0530

    Login: invalid request response message change

--- src/routes/login.js
+++ src/routes/login.js
@@ -4,7 +4,6 @@ const verifyPassword = require('../utils/helpers/verifyPassword');
 const fetchUserDetails = require('../utils/helpers/fetchUserDetails');
 const createToken = require('../utils/helpers/createToken');
 
-
 module.exports = [
   {
     method: 'POST',
@@ -12,31 +11,32 @@ module.exports = [
     handler: (Request, Response) => {
       const userData = getUserFormPayload(Request);
       if (joiLoginValidation(userData) === false) {
-        Response('Invalid User Data').code(422);
+        Response({
+          code: 409,
+          message: 'Invalid Request',
+        });
       } else {
         fetchUserDetails(userData.email)
           .then((result) => {
-            if (result === null) {
+            if (result !== null && verifyPassword(userData.password, result.dataValues.password)) {
               Response({
-                code: 409,
-                message: 'User Not Registered',
+                code: 200,
+                token: createToken(result.dataValues),
+                message: 'Logged in',
+                username: result.dataValues.fullName,
               });
             } else {
-              const passwordMatch = verifyPassword(userData.password, result.dataValues.password);
-              if (passwordMatch === false) {
-                Response({
-                  code: 422,
-                  message: 'Invalid Password',
-                });
-              } else {
-                Response({
-                  code: 200,
-                  token: createToken(result.dataValues),
-                  message: 'Logged in',
-                  username: result.dataValues.fullName,
-                });
-              }
+              Response({
+                code: 409,
+                message: 'Invalid Request',
+              });
             }
+          })
+          .catch(() => {
+            Response({
+              code: 500,
+              message: 'Internal Server Error',
+            });
           });
       }
     },
