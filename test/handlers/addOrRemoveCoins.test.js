66b79890f8450833c3569dbae18e6469d6a21b6b
parth <srivastavaparth2016@gmail.com>
2018-03-10 19:04:03 +0530

    adding test case to check if the coin id being inserted is correct

---
+++ test/handlers/addOrRemoveCoins.test.js
@@ -0,0 +1,40 @@
+const addOrRemoveCoins = require('../../src/handlers/addOrRemoveCoins');
+const Models = require('../../models');
+const getCoinsArray = require('../../src/utils/helpers/getCoinsArrayforSeed');
+
+describe('testing whether the addOrRemoveCoin function works or not', () => {
+  beforeAll(done => Models.users.upsert({
+    id: 1,
+    fullName: 'crypto',
+    mobileNumbe: 9876543210,
+    email: 'crypto@gmail.com',
+    password: 'helloCrypto',
+  }).then(() => Models.users.upsert({
+    id: 2,
+    fullName: 'parth',
+    mobileNumbe: 9876547650,
+    email: 'parth@gmail.com',
+    password: 'helloC[arth]o',
+  }).then(() =>
+    Models.coins.bulkCreate(getCoinsArray()).then(() => done()))));
+
+  afterAll((done) => {
+    Models.users.destroy({ truncate: true, cascade: true }).then(() =>
+      Models.coins.destroy({ truncate: true, cascade: true }).then(() =>
+        Models.transactions.destroy({ truncate: true, cascade: true }).then(() => done())));
+  });
+  afterEach((done) => {
+    Models.transactions.destroy({ truncate: true, cascade: true }).then(() => {
+      done();
+    });
+  });
+
+  test('checking whether transaction is being inserted with the right coin id', (done) => {
+    addOrRemoveCoins(2, 'BTC', 12345, 23).then((result) => {
+      expect(result.coinId).toBe(1);
+    }).then(() => {
+      done();
+    });
+  });
+});
+
