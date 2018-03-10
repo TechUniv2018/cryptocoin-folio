7430c4f10c6b5cffaf5e5fff10e914dca817ad8e
parth <srivastavaparth2016@gmail.com>
2018-03-11 01:24:59 +0530

    editTranscation route added

---
+++ src/routes/editTransaction.js
@@ -0,0 +1,32 @@
+const getJWTpayload = require('../utils/helpers/getJWTpayload');
+const deleteTransaction = require('../../src/utils/helpers/deleteTransaction');
+const editTransaction = require('../../src/utils/helpers/editTransaction');
+
+module.exports = {
+  method: 'POST',
+  path: '/editTransaction',
+  handler: (request, response) => {
+    const userData = getJWTpayload(request);
+    if (userData.message === 'token expired') {
+      response({ message: 'Token Expired' }).code(401);
+    } else {
+      const { userId } = userData;
+      const queryObject = request.query;
+      const typeOfRequest = Object.keys(queryObject);
+      if (typeOfRequest[0] === 'delete') {
+        deleteTransaction(userId, queryObject[typeOfRequest[0]]).then((reply) => {
+          response(reply);
+        });
+      } else {
+        const { price } = request.payload;
+        const { quantity } = request.payload;
+        const { coinId } = request.payload;
+        editTransaction(userId, queryObject[typeOfRequest[0]], price, quantity, coinId)
+          .then((reply) => {
+            response(reply);
+          });
+      }
+    }
+  },
+};
+
