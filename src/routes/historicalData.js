3309be98f97b22a97099eb325f6b6d98c74f3e3d
Rahul Kumar <rahulcomp24@gmail.com>
2018-03-13 06:11:26 +0530

    Historical Data : Add search by coin ID

--- src/routes/historicalData.js
+++ src/routes/historicalData.js
@@ -23,27 +23,30 @@ module.exports = [
     method: 'GET',
     handler: (request, response) => {
       const coin = request.query.coin || 'BTC';
-      Models.prices.findAll({
-        attributes: ['timeStamp', 'open', 'close', 'high', 'low'],
-        where: { coinId: 16 },
-        raw: true,
-        order: [['timeStamp', 'ASC']],
-      }).then((priceResult) => {
-        const priceArray = priceResult.map(price => Object.values(price));
-        Models.prices.findAll({
-          attributes: ['timeStamp', 'volume'],
-          where: { coinId: 16 },
-          raw: true,
-          order: [['timeStamp', 'ASC']],
-        }).then((volumeResult) => {
-          const volumeArray = volumeResult.map(price => Object.values(price));
-          for (let i = 0; i < volumeArray.length; i += 1) {
-            priceArray[i][0] = Number(priceArray[i][0]) * 1000;
-            volumeArray[i][0] = Number(volumeArray[i][0]) * 1000;
-          }
-          response({ price: priceArray, volume: volumeArray });
+      Models.coins.findOne({ where: { symbol: coin }, raw: true })
+        .then((result) => {
+          Models.prices.findAll({
+            attributes: ['timeStamp', 'open', 'close', 'high', 'low'],
+            where: { coinId: result.id },
+            raw: true,
+            order: [['timeStamp', 'ASC']],
+          }).then((priceResult) => {
+            const priceArray = priceResult.map(price => Object.values(price));
+            Models.prices.findAll({
+              attributes: ['timeStamp', 'volume'],
+              where: { coinId: result.id },
+              raw: true,
+              order: [['timeStamp', 'ASC']],
+            }).then((volumeResult) => {
+              const volumeArray = volumeResult.map(price => Object.values(price));
+              for (let i = 0; i < volumeArray.length; i += 1) {
+                priceArray[i][0] = Number(priceArray[i][0]) * 1000;
+                volumeArray[i][0] = Number(volumeArray[i][0]) * 1000;
+              }
+              response({ price: priceArray, volume: volumeArray });
+            });
+          });
         });
-      });
     },
   },
 ];
