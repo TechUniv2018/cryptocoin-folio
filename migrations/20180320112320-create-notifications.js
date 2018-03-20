ad85984969e85f81842cf45330370dd9559b3cc1
Ram Prasad Agarwal <ramprasadagarwal313@gmail.com>
2018-03-20 17:07:30 +0530

    notification: migration created

---
+++ migrations/20180320112320-create-notifications.js
@@ -0,0 +1,28 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.createTable('notifications', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER,
+    },
+    userId: {
+      type: Sequelize.INTEGER,
+    },
+    text: {
+      type: Sequelize.STRING,
+    },
+    status: {
+      type: Sequelize.BOOLEAN,
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+  }),
+  down: (queryInterface, Sequelize) => queryInterface.dropTable('notifications'),
+};
