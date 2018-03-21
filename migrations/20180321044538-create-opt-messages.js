8dea6d8c304ba7a6031bb26cc30cb6cee5b11efb
ShubhamZanwar <zanwar.shubham@gmail.com>
2018-03-21 10:34:22 +0530

    otpMessages: Adding the table to contain the OTP messages

---
+++ migrations/20180321044538-create-opt-messages.js
@@ -0,0 +1,27 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.createTable('otpMessages', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER,
+    },
+    userId: {
+      type: Sequelize.INTEGER,
+      unique: true,
+      references: { model: 'users', key: 'id' },
+    },
+    otp: {
+      type: Sequelize.INTEGER,
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+  }),
+  down: (queryInterface, Sequelize) => queryInterface.dropTable('otpMessages'),
+};
