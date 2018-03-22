ec89318014a91c8cc466f4630f90f36688e68a9d
ShubhamZanwar <zanwar.shubham@gmail.com>
2018-03-22 17:34:40 +0530

    Otp: Path for generating OTP

--- src/routes/otp.js
+++ src/routes/otp.js
@@ -1,14 +1,25 @@
 const getJWTpayload = require('../utils/helpers/getJWTpayload');
+const generateOtp = require('../utils/helpers/generateOtp');
+const Models = require('../../models');
 
 module.exports = [{
-  method: 'GET',
+  method: 'PUT',
   path: '/otp',
   handler: (request, response) => {
     const userData = getJWTpayload(request);
     if (userData.message === 'token expired') {
       response({ message: 'Token Expired' }).code(401);
     } else {
-      response('OTP').code(200);
+      const { userId } = userData;
+      const otp = generateOtp();
+      Models.otpMessages.upsert({
+        userId,
+        otp,
+      }).then(() => {
+        response().code(200);
+      }, () => {
+        response().code(500);
+      });
     }
   },
 }];
--- test/routes/otp.test.js
+++ test/routes/otp.test.js
@@ -3,10 +3,28 @@ const Models = require('../../models');
 const createToken = require('../../src/utils/helpers/createToken');
 
 describe('Testing the GET OTP route', () => {
+  beforeEach((done) => {
+    Models.users.create({
+      id: 2,
+      email: 'paul@gmail.com',
+      password: 'paul123',
+      fullName: 'Paul',
+      mobileNumbe: '9944198887',
+    }).then(done)
+      .catch(console.log);
+  });
+  afterEach((done) => {
+    Models.users.destroy({
+      truncate: true,
+      cascade: true,
+    })
+      .then(done)
+      .catch(console.log);
+  });
   test('The route should repond with 200 for a valid username', (done) => {
     const options = {
       url: '/otp',
-      method: 'GET',
+      method: 'PUT',
       headers: {
         authToken: createToken({ id: 2 }),
       },
@@ -19,7 +37,7 @@ describe('Testing the GET OTP route', () => {
   test('The route should respond with a 401 if the user is not logged in', (done) => {
     const options = {
       url: '/otp',
-      method: 'GET',
+      method: 'PUT',
       headers: {
         authToken: 'abcdddd',
       },
@@ -27,6 +45,27 @@ describe('Testing the GET OTP route', () => {
     server.inject(options, (result) => {
       expect(result.statusCode).toBe(401);
       done();
+    });
+  });
+  test('The route should create an entry in the database against the userID with the OTP generated', (done) => {
+    const options = {
+      url: '/otp',
+      method: 'PUT',
+      headers: {
+        authToken: createToken({ id: 2 }),
+      },
+    };
+    server.inject(options, (result) => {
+      Models.otpMessages.findOne({
+        where: {
+          userId: 2,
+        },
+        raw: true,
+      }).then((otp) => {
+        console.log(otp);
+        expect(true).toBe(true);
+        done();
+      });
     });
   });
 });
